2e7b26ebcfa88ae6165a51a4dcdc6b73b49f93cf
legendary-cookie <vincent.schweiger@xolley.de>
2022-02-13 18:14:53 +0100

    polybar script

--- dot_config/polybar/executable_updates-pacman.sh
+++ dot_config/polybar/executable_updates-pacman.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [[ ! $? -eq 0 ]]; then
+  echo "Offline"
+  exit
+fi
 
 if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
     updates=0
